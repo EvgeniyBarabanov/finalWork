8b26d9ce82f74e4b77bba6d2ead550e0eaf66b57
EyheniyBarabanov <eyheniy.barabanov.96@mail.ru>
2022-08-09 22:23:20 +0300

    burgerMenu is work

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,18 +1,26 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import style from './Nav.module.css';
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
 
 function Nav() {
+
+	const visibilityMenu = useRef();
+
+	const visibility = function(){
+		visibilityMenu.current.classList.toggle(style.visibilityMenu);
+	}
+
 	return (
 	<nav className={style.block}>
-		<button className={style.block__burgerBtn}></button>
-		<div className={style.logo}>
+		<button className={style.block__burgerBtn} onClick={visibility}>X</button>
+		<div className={style.block__logo}>
         	    <a href="/">
         	        <img src={PUBLIC_URL + '/images/logo.png'} alt="#" />
         	    </a>
         	</div>
-	    <ul className={style.list}>
+	    <ul ref={visibilityMenu} className={style.block__list}>
 			<li><Link to="/rulesPay">Как покупать</Link></li>
 			<li><Link to="/sale">Распродажи</Link></li>
 			<li><Link to="/prices">Цены</Link></li>
@@ -21,9 +29,9 @@ function Nav() {
 			<li><Link to="/blog">Блог</Link></li>
 			<li><Link to="/stores">Магазины</Link></li>	
 		</ul>
-		<div className={style.user}>
-			<Link to="/login" className={style.logIn}><img src={PUBLIC_URL + '/images/userLogo.png'} alt="#" /> Вход</Link>
-			<button className={style.contact_btn}>Связаться с нами</button>
+		<div className={style.block__user}>
+			<Link to="/login" className={style.block__logIn}><img src={PUBLIC_URL + '/images/userLogo.png'} alt="#" /> Вход</Link>
+			<button className={style.block__contact_btn}>Связаться с нами</button>
 		</div>
 	</nav>
 	);
