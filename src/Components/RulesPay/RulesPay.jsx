9675ae2b1434091631311a238aa1702f2e455165
EyheniyBarabanov <eyheniy.barabanov.96@mail.ru>
2022-08-24 00:13:02 +0300

    23 08 rulesPay

--- src/Components/RulesPay/RulesPay.jsx
+++ src/Components/RulesPay/RulesPay.jsx
@@ -1,8 +1,66 @@
+import style from './RulesPay.module.css';
+
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
 function RulesPay() {
 	return (
-	<div className="rulesPay">
-	    RulesPay
-	</div>
+		<section className="RulesPay">
+		    <div className="container">
+				{/* <div className={style.wrapper}> */}
+					<div className={style.promo}>
+						<div className={style.promo_info}>
+							<p>Главная / Как покупать</p>
+							<h2>Научим покупать в Великобритании! </h2>
+							<span>и экономить до 70%</span>
+						</div>
+						<div className={style.promo_btn}>
+							<button>Получить адрес в UK</button>
+						</div>
+					</div>
+					<div className={style.stepBuy}>
+						<h2>Покупать в Великобритании очень просто</h2>
+						<p>Английские магазины не доставляют в Россию, но это не проблема. С OkeyPost можно покупать по лучшим ценам и экономить на доставке. Мы получим ваши посылки, надежно упакуем и отправим их к вам домой.</p>
+						<div className={style.stepBuy_info}>
+							<div className={style.stepBuy_info_stage}>
+								<img src={PUBLIC_URL + '/images/stepBuy1.png'} alt="#" />
+								<div className={style.stepBuy_info_stage_wrapper}>
+									<h3>Получаем адрес для покупок в магазинах Великобритании </h3>
+									<p>Okeypost поможет притвориться американцем и не переплачивать. Для этого нужно зарегистрироваться в нашем сервисе. После регистрации вам станут доступны два адреса в Англии, которые вы сможете указывать при оформлении заказов в интернет-магазинах.</p>
+								</div>
+							</div>
+							<div className={style.stepBuy_info_stage}>
+								<img src={PUBLIC_URL + '/images/stepBuy2.png'} alt="#" />
+								<div className={style.stepBuy_info_stage_wrapper}>
+									<h3>Получаем адрес для покупок в магазинах Великобритании </h3>
+									<p>Okeypost поможет притвориться американцем и не переплачивать. Для этого нужно зарегистрироваться в нашем сервисе. После регистрации вам станут доступны два адреса в Англии, которые вы сможете указывать при оформлении заказов в интернет-магазинах.</p>
+								</div>
+							</div>
+							<div className={style.stepBuy_info_stage}>
+								<img src={PUBLIC_URL + '/images/stepBuy3.png'} alt="#" />
+								<div className={style.stepBuy_info_stage_wrapper}>
+									<h3>Получаем адрес для покупок в магазинах Великобритании </h3>
+									<p>Okeypost поможет притвориться американцем и не переплачивать. Для этого нужно зарегистрироваться в нашем сервисе. После регистрации вам станут доступны два адреса в Англии, которые вы сможете указывать при оформлении заказов в интернет-магазинах.</p>
+								</div>
+							</div>
+							<div className={style.stepBuy_info_stage}>
+								<img src={PUBLIC_URL + '/images/stepBuy4.png'} alt="#" />
+								<div className={style.stepBuy_info_stage_wrapper}>
+									<h3>Получаем адрес для покупок в магазинах Великобритании </h3>
+									<p>Okeypost поможет притвориться американцем и не переплачивать. Для этого нужно зарегистрироваться в нашем сервисе. После регистрации вам станут доступны два адреса в Англии, которые вы сможете указывать при оформлении заказов в интернет-магазинах.</p>
+								</div>
+							</div>
+							<div className={style.stepBuy_info_stage}>
+								<img src={PUBLIC_URL + '/images/stepBuy5.png'} alt="#" />
+								<div className={style.stepBuy_info_stage_wrapper}>
+									<h3>Получаем адрес для покупок в магазинах Великобритании </h3>
+									<p>Okeypost поможет притвориться американцем и не переплачивать. Для этого нужно зарегистрироваться в нашем сервисе. После регистрации вам станут доступны два адреса в Англии, которые вы сможете указывать при оформлении заказов в интернет-магазинах.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				{/* </div> */}
+			</div>
+		</section>
 	);
 }
 
