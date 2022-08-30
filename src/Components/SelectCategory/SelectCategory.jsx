cfe3f51ce32574be04d5a91d217e192c25eb8545
EyheniyBarabanov <eyheniy.barabanov.96@mail.ru>
2022-08-30 23:25:54 +0300

    30 08 slider

--- src/Components/SelectCategory/SelectCategory.jsx
+++ src/Components/SelectCategory/SelectCategory.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useContext, useRef } from 'react';
 import { MainContext } from '../Dashboard/Dashboard';
 
@@ -10,35 +12,40 @@ const PUBLIC_URL = process.env.PUBLIC_URL;
 function SelectCategory(props) {
 	const {allProducts} = useContext(MainContext);
 
+	const [cartChecked, setCartChecked] = useState([])
+
 	const direction = useRef();
-	
+
+
 	const changeSlide = function(event){
 		let dir = event.target.id;
+
 		if(dir === ''){
 			dir = event.target.parentNode.id
 		}
 		if(dir === "next"){
 			let firstChild = direction.current.firstChild;
 			direction.current.append(firstChild);
-			/* for (let child of direction.current.children[1].children[1]){
-				direction.current.children[1].children[1].firstChild.classList.remove(style.prewSlide);
-				direction.current.children[1].children[1].firstChild.classList.add(style.nextSlide);
-			}  */
-		}else if((dir === "prew")){
+			for(let child of direction.current.children[1].children[1].children){
+				child.classList.remove(style.prewSlide);
+				child.classList.add(style.nextSlide);
+			}
+		}else{
 			let lastChild = direction.current.lastChild;
 			direction.current.prepend(lastChild);
-			/* for (let child of direction.current.children[1].children[1]){
+			for(let child of direction.current.children[1].children[1].children){
 				child.classList.remove(style.nextSlide);
-				child.classList.add(style.prewSlide)
-			} */
+				child.classList.add(style.prewSlide);
+			}
 		}
 	}
 
 	const addToCart = function(){
 		let currentProduct = direction.current.firstChild.nextSibling;
+		let idProduct = [currentProduct.id];
 		if(!getCookie("id")){
 			document.cookie = 'id=' + currentProduct.id + '; max-age=1800';
-			console.log(getCookie("id"));
+			setCartChecked([...idProduct]);
 		}else{
 			let arr = (getCookie("id").match(/\d+/g));
 			if(arr.includes(currentProduct.id)){
@@ -46,9 +53,15 @@ function SelectCategory(props) {
 			}else{
 				arr.push(currentProduct.id);
 				document.cookie = 'id=' + arr + '; max-age=1800';
+				setCartChecked([...arr]);
 			}
 		}
 	}
+
+
+	/* useEffect(()=>{
+		test()
+	}) */
 
 	return (
 		<div className="selectCategory">
@@ -62,23 +75,25 @@ function SelectCategory(props) {
 										<li key={id} id={item.id} className={style.sliderProduct__item}>
 											<button id='prew' onClick={changeSlide} className={style.btn_prew}><img src={PUBLIC_URL + '/images/arrowLeft.png'} alt="#" /></button>
 												<div className={style.sliderProduct__item_wrapper}>
-													<div className={`${style.images_container} ${style.nextSlide}`}>
-														<img className={style.image} src={item.images[0]} alt="#" />
-													</div>
+													<div className={style.sliderProduct__item_wrapper_elem}>
+														<div className={style.images_container}>
+															<img className={style.image} src={item.images[0]} alt="#" />
+														</div>
 
-													<div className={style.sliderProduct__item_info}>
-														<div className={style.sliderProduct__item_info_title}>
-															<p>{item.title}</p>
-															<p>{item.price + "$"}</p>
+														<div className={style.sliderProduct__item_info}>
+															<div className={style.sliderProduct__item_info_title}>
+																<p>{item.title}</p>
+																<p>{item.price + "$"}</p>
+															</div>
+															<div className={style.sliderProduct__item_info_rating}>
+																<div style={{background: `linear-gradient(90deg, GreenYellow ${item.rating/0.05}%, #ffffff 0%`}} className={style.images_mask}></div>
+																<img className={style.sliderProduct_item_info_stars} style={{backgroundColor: 'transparent'}} src={PUBLIC_URL + '/images/stars.png'} alt="#" />
+															</div>
+															<div className={style.sliderProduct__item_info_description}>
+																<p>{item.description}</p>
+															</div>
+															<button onClick={addToCart} className={style.sliderProduct__addToCart}>Добавить в корзину</button>
 														</div>
-														<div className={style.sliderProduct__item_info_rating}>
-															<div style={{background: `linear-gradient(90deg, GreenYellow ${item.rating/0.05}%, #ffffff 0%`}} className={style.images_mask}></div>
-															<img className={style.sliderProduct_item_info_stars} style={{backgroundColor: 'transparent'}} src={PUBLIC_URL + '/images/stars.png'} alt="#" />
-														</div>
-														<div className={style.sliderProduct__item_info_description}>
-															<p>{item.description}</p>
-														</div>
-														<button onClick={addToCart} className={style.sliderProduct__addToCart}>добавить в корзину</button>
 													</div>
 												</div>
 											<button id='next' onClick={changeSlide} className={style.btn_next}><img src={PUBLIC_URL + '/images/arrowRight.png'} alt="#" /></button>
